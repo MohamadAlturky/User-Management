53d4d1896e2b16e2d36b66ddde3aebce51832bdb
mohamad.alturky <m799678@gmail.com>
2024-06-09 23:59:52 +0300

    Adding registrarion mechanism for adding services to DI container

---
+++ Donut.SharedKernel/DependencyInjection/Scanner/DependencyInjectionScanner.cs
@@ -0,0 +1,24 @@
+﻿using Donut.SharedKernel.DependencyInjection.Installer;
+using Donut.SharedKernel.Utilities;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Donut.SharedKernel.DependencyInjection.Scanner;
+
+public static class DependencyInjectionScanner
+{
+    public static IServiceCollection RegisterServices(this IServiceCollection services,IConfiguration configuration,params Assembly[] assemblies)
+    {
+        var installers = assemblies.SelectMany(assembly => assembly.DefinedTypes)
+                  .Where(TypeChecker.IsAssignableToType<IDependencyInjectionInstaller>)
+                  .Select(Activator.CreateInstance)
+                  .Cast<IDependencyInjectionInstaller>();
+
+        foreach (var installer in installers)
+        {
+            installer.Install(services,configuration);
+        }
+        return services;
+    }
+}
