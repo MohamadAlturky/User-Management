50b159cae25a633b900b53e0d5613af3a86712b7
mohamad.alturky <m799678@gmail.com>
2024-06-10 01:42:32 +0300

    Result Pattern

---
+++ Donut.SharedKernel/Results/Error.cs
@@ -0,0 +1,15 @@
+﻿namespace Donut.SharedKernel.Results;
+
+public class Error
+{
+
+    public static readonly Error None = new(string.Empty, string.Empty);
+    public static readonly Error Null = new("No Data Found From Domain !!", "The Result Value Is Null");
+    public Error(string code, string message)
+    {
+        Code = code;
+        Message = message;
+    }
+    public string Code { get; set; }
+    public string Message { get; set; }
+}
---
+++ Donut.SharedKernel/Results/Result.cs
@@ -0,0 +1,68 @@
+﻿namespace Donut.SharedKernel.Results;
+
+public class Result
+{
+    protected internal Result(bool isSuccess, Error error)
+    {
+
+        if (isSuccess && error != Error.None)
+        {
+            throw new InvalidOperationException();
+        }
+
+        if (!isSuccess && error == Error.None)
+        {
+            throw new InvalidOperationException();
+        }
+
+        IsSuccess = isSuccess;
+        Error = error;
+    }
+    public bool IsSuccess { get; }
+    public bool IsFailure => !IsSuccess;
+    public Error Error { get; set; }
+
+
+
+    public static Result Success() => new Result(true, Error.None);
+    public static Result<T> Success<T>(T value)
+    {
+        return new Result<T>(value, true, Error.None);
+    }
+
+
+
+    public static Result Failure(Error error) => new Result(false, error);
+    public static Result<T> Failure<T>(Error error) => new Result<T>(error);
+
+
+    public static Result<T> Create<T>(T? value)
+    {
+        if (value != null)
+        {
+            return Result.Success(value);
+        }
+        else
+        {
+            return Result.Failure<T>(Error.Null);
+        }
+    }
+}
+public class Result<T> : Result
+{
+    private T _value;
+    protected internal Result(T value, bool isSuccess, Error error)
+        : base(isSuccess, error)
+    {
+        _value = value;
+    }
+
+    protected internal Result(Error error)
+        : base(false, error)
+    {
+        _value = default;
+    }
+
+    public T Value => IsSuccess ? _value :
+        throw new InvalidOperationException("The value of a failure result can't be accessed.");
+}
