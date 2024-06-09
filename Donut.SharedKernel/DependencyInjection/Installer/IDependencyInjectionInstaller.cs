2975ffd3bc7bef346c2dcf4f5cefb895e86ed8b2
mohamad.alturky <m799678@gmail.com>
2024-06-09 23:59:20 +0300

    di installer abstraction that will be implemented in all projects

---
+++ Donut.SharedKernel/DependencyInjection/Installer/IDependencyInjectionInstaller.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Donut.SharedKernel.DependencyInjection.Installer;
+
+public interface IDependencyInjectionInstaller
+{
+    void Install(IServiceCollection services, IConfiguration configuration);
+}
