28bef32f4c01bef0035f706b16e0d98a85cb76f3
mohamad.alturky <m799678@gmail.com>
2024-06-10 00:02:14 +0300

    Adding Domain Exception

---
+++ Donut.SharedKernel/Exceptions/DomainException.cs
@@ -0,0 +1,13 @@
+﻿namespace Donut.SharedKernel.Exceptions;
+
+public class DomainException : Exception
+{
+    public DomainException()
+    {
+    }
+
+    public DomainException(string message) : base(message)
+    {
+    }
+
+}
