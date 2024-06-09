937aa759e4ab1fb3ea07595adc9661890cd6229b
mohamad.alturky <m799678@gmail.com>
2024-06-10 00:00:07 +0300

    Adding Utils

---
+++ Donut.SharedKernel/Utilities/TypeChecker.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+
+namespace Donut.SharedKernel.Utilities;
+
+public static class TypeChecker
+{
+    public static bool IsAssignableToType<T>(TypeInfo info)
+    {
+        return typeof(T).IsAssignableFrom(info) && !info.IsAbstract && !info.IsInterface;
+    }
+}
