33db0807e668fdfe6b8149644da06b4efc3cf90a
mohamad.alturky <m799678@gmail.com>
2024-06-10 16:19:02 +0300

    DI config

---
+++ Donut.MemberShip.Authentication/Configuration/AuthenticationServiceInstaller.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Donut.MemberShip.Core.DatabaseContext;
+using Donut.MemberShip.Core.Models;
+using Donut.SharedKernel.DependencyInjection.Installer;
+using Microsoft.Extensions.Configuration;
+
+namespace Donut.MemberShip.Authentication.Configuration;
+
+public class AuthenticationServiceInstaller : IDependencyInjectionInstaller
+{
+    public int PriorityLevel => 1;
+
+    public void Install(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddAuthentication().AddBearerToken(IdentityConstants.BearerScheme);
+        services.AddAuthorizationBuilder();
+        //services.AddIdentityApiEndpoints<ApplicationUser>()
+        //            .AddEntityFrameworkStores<MemberShipDbContext>();
+
+        services.AddIdentityCore<ApplicationUser>()
+            .AddEntityFrameworkStores<MemberShipDbContext>()
+            .AddApiEndpoints();
+    }
+}
