d17d437ea0e0ea7f799ac0596b272c2410fc4b51
mohamad.alturky <m799678@gmail.com>
2024-06-10 16:20:04 +0300

    Login EndPoints

---
+++ Donut.MemberShip.Authentication/EndPoints/Routes/LoginEndPoint.cs
@@ -0,0 +1,54 @@
+﻿using Donut.MemberShip.Authentication.EndPoints.Installer;
+using Donut.MemberShip.Authentication.EndPoints.Settings;
+using Donut.MemberShip.Core.Models;
+using Microsoft.AspNetCore.Authentication.BearerToken;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Donut.MemberShip.Authentication.EndPoints.Routes;
+
+public class LoginEndPoint : IEndPointInstaller
+{ 
+    public void AddRoutes(IEndpointRouteBuilder endpoints)
+    {
+        var routeGroup = endpoints.MapGroup(RouteSettings.API_ROUTE_GROUP);
+
+        routeGroup.MapPost("/login", async Task<Results<Ok<AccessTokenResponse>, EmptyHttpResult, ProblemHttpResult>>
+            ([FromBody] LoginRequest login, [FromQuery] bool? useCookies, [FromQuery] bool? useSessionCookies, [FromServices] IServiceProvider sp) =>
+        {
+            var signInManager = sp.GetRequiredService<SignInManager<ApplicationUser>>();
+
+            var useCookieScheme = useCookies == true || useSessionCookies == true;
+            var isPersistent = useCookies == true && useSessionCookies != true;
+            signInManager.AuthenticationScheme = useCookieScheme ? IdentityConstants.ApplicationScheme : IdentityConstants.BearerScheme;
+
+            var result = await signInManager.PasswordSignInAsync(login.Email, login.Password, isPersistent, lockoutOnFailure: true);
+
+            if (result.RequiresTwoFactor)
+            {
+                if (!string.IsNullOrEmpty(login.TwoFactorCode))
+                {
+                    result = await signInManager.TwoFactorAuthenticatorSignInAsync(login.TwoFactorCode, isPersistent, rememberClient: isPersistent);
+                }
+                else if (!string.IsNullOrEmpty(login.TwoFactorRecoveryCode))
+                {
+                    result = await signInManager.TwoFactorRecoveryCodeSignInAsync(login.TwoFactorRecoveryCode);
+                }
+            }
+
+            if (!result.Succeeded)
+            {
+                return TypedResults.Problem(result.ToString(), statusCode: StatusCodes.Status401Unauthorized);
+            }
+
+            // The signInManager already produced the needed response in the form of a cookie or bearer token.
+            return TypedResults.Empty;
+        });
+    }
+}
