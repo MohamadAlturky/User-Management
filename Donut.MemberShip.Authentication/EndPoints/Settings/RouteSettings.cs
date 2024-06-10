245a08edd03fc7c74d0119d6635bdb01e5f707c3
mohamad.alturky <m799678@gmail.com>
2024-06-10 16:22:20 +0300

    EndPoints Routes Settings

---
+++ Donut.MemberShip.Authentication/EndPoints/Settings/RouteSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Donut.MemberShip.Authentication.EndPoints.Settings;
+
+internal class RouteSettings
+{
+    public static string API_ROUTE_GROUP = "/auth";
+    public static string JWT_ROUTE_GROUP = "/jwt";
+    public static string PASSWORD_ROUTE_GROUP = "/password";
+    public static string EMAIL_ROUTE_GROUP = "/email";
+    public static string MANAGE_ROUTE_GROUP = "/manage";
+}
