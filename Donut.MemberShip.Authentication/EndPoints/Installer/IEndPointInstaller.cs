ff5074b09a4182a5b2b09e18e443a1c37001b5fd
mohamad.alturky <m799678@gmail.com>
2024-06-10 16:19:26 +0300

    EndPoints installer abstraction

---
+++ Donut.MemberShip.Authentication/EndPoints/Installer/IEndPointInstaller.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Routing;
+
+namespace Donut.MemberShip.Authentication.EndPoints.Installer;
+
+public interface IEndPointInstaller
+{
+    public void AddRoutes(IEndpointRouteBuilder endpoints);
+}
